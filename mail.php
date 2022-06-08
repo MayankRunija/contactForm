2219887b9e5be29a35c7aa04d438ef6f44b1c19e
Mayank Runija <84033122+MayankRunija@users.noreply.github.com>
2022-06-08 08:11:31 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -7,7 +7,7 @@ $message = $_POST['message'];
 
 $mailheader = "From:".$name."<".$email.">\r\n";
 
-$recipient = "example@gmail.com";
+$recipient = "mickeyruni18@gmail.com";
 
 mail($recipient, $subject, $message, $mailheader) or die("Error!");
 
